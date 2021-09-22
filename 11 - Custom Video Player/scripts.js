e996822f94b58a432c9fbe3b88b03254f0b42eb6
Jalynn Ridley <jallywally@gmail.com>
2021-09-22 11:07:37 +0200

    added scrub to slider

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -43,6 +43,12 @@ function handleProgress() {
     progressBar.style.flexBasis = `${percent}%`;
 }
 
+// moving video time to click in progress bar
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration; // where click is on x axis divided by width of progress bar * how long the video is
+    video.currentTime = scrubTime;
+}
+
 // hook up event listeners
 video.addEventListener('click', togglePlay); // clicking on screen, not control bar
 video.addEventListener('play', updateButton); // changing icon of button when played
@@ -54,3 +60,10 @@ toggle.addEventListener('click', togglePlay); // clicking on play in control bar
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate)); // listen for all ranges changes (sliders)
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate)); // listen for all ranges changes (sliders)
+
+// create flag variable for progress  slider click and hold
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', e => mousedown && scrub(e)); // pass the event (e), check if mousedown is true, if it is, run scrb with the event,  else, returns false and does nothing
+progress.addEventListener('mousedown', () => (mousedown = true));
+progress.addEventListener('mouseup', () => (mousedown = false));
